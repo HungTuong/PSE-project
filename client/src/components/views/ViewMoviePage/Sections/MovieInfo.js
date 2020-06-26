b820abaab40d8b4e2df20a452c973d4a421085b2
HungTuong <42890074+HungTuong@users.noreply.github.com>
2020-06-26 09:54:47 +0700

    Update movie info

--- client/src/components/views/ViewMoviePage/Sections/MovieInfo.js
+++ client/src/components/views/ViewMoviePage/Sections/MovieInfo.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Descriptions } from 'antd';
 
-function MovieInfo() {
+function MovieInfo(props) {
+    const [Movie, setMovie] = useState({})
+
+    useEffect(() => {
+
+        setMovie(props.detail)
+
+    }, [props.detail])
+
     return (
         <div>
-            hello
+            <Descriptions bordered layout="vertical" title="Movie Info">
+                <Descriptions.Item label="Genre"> {Movie.genre}</Descriptions.Item>
+                <Descriptions.Item label="Length"> {Movie.length} mins</Descriptions.Item>
+                <Descriptions.Item label="Description"> {Movie.description}</Descriptions.Item>
+            </Descriptions>
         </div>
     )
 }
